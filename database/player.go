34d4e9e6fc9e579c43eb74b3940d73977cd5609b
Adam Zahradník <adam@zahradnik.xyz>
2022-04-26 11:23:25 +0200

    add player pretty names

--- database/player.go
+++ database/player.go
@@ -3,10 +3,19 @@ package database
 import "time"
 
 type Player struct {
-	ID       uint `gorm:"primaryKey"`
-	Name     string
-	Password string
-	Score    int
+	ID          uint `gorm:"primaryKey"`
+	Name        string
+	Password    string
+	Score       int `gorm:"default:0"`
+	DisplayName string
+}
+
+func (p Player) PrettyName() string {
+	if p.DisplayName != "" {
+		return p.DisplayName
+	}
+
+	return p.Name
 }
 
 type PlayerVersion struct {
